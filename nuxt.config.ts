998a9c19a33009e7b8447979527ca0d3927d2d5c
Maiguelman <miguel.cassimiro99@gmail.com>
2023-10-24 22:03:32 -0300

    feat(storyblok): initial connection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+        accessToken: `${process.env.STORYBLOK_SECRET_KEY}`,
+        apiOptions: {
+          region: 'us',
+        },
+      },
+    ],
+  ],
 })
