80cfd4f979240944fda9468b18ecc50e7b523a31
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2023-12-22 23:45:58 -0300

    feat: refactor on components

--- storyblok/SectionTitle.vue
+++ storyblok/SectionTitle.vue
@@ -1,12 +1,12 @@
 <script setup>
-  defineProps({ title: String })
+defineProps({ blok: Object })
 </script>
 <template>
   <div class="text-center flex w-full justify-center">
-    <h3 class="text-2xl text-primary-dark font-tilt font-bold border-b-2 pb-2 border-secondary-dark">
-      {{ title }}
+    <h3
+      class="text-2xl text-primary-dark font-tilt font-bold border-b-2 pb-2 border-secondary-dark">
+      {{ blok.title }}
     </h3>
   </div>
 </template>
-<style scoped>
-</style>
+<style scoped></style>
